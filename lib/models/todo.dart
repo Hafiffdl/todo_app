8a87cc661b2a30c1f75b277c92274d5da812b84f
Hafiffdl <hafif.fdl16@gmail.com>
2023-11-21 14:20:42 +0700

    add: models todo

---
+++ lib/models/todo.dart
@@ -0,0 +1,20 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone =false,
+  });
+
+  static List<ToDo> todoList() {
+    return[
+      
+      ToDo(id: '01', todoText: 'Tugas Mobile Programming',),
+      ToDo(id: '02', todoText: 'Membeli Minyak',),
+      ToDo(id: '03', todoText: 'Olahraga',)
+    ];
+  }
+}
